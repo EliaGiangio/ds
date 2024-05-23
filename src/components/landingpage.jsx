f057c4bb8bb3deab4ee49cf4c2afa70da77428c2
Elia Giangiordano <60135121+EliaGiangio@users.noreply.github.com>
2024-05-23 12:17:32 +0200

    Update landingpage.jsx

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
                     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Aspiring to become a competitive weightlifter or just want to try out? Come join our community!</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                         <a className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" onClick={() => navigate('/ds-new-site/#pricing')}>
-                            Book a session
+                            Book a sesh
                             <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
