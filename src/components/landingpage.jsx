7261063d2a2ab5b136cc1b491d748cd947f7d3d9
EliaGiangio <eliagiang@yahoo.com>
2024-06-10 12:49:51 +0200

    Added shop landing page and adjusted some margins

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -10,13 +10,13 @@ export default function LandingPage() {
     };
     return (
         <>
-            <section className="bg-center bg-no-repeat bg-[url('https://res.cloudinary.com/dggfjc8rb/image/upload/v1716289115/Dutch%20Strength/DS_Wide_ij2tvb.jpg')] bg-gray-700 bg-blend-multiply bg-cover">
+            <section className="bg-center bg-no-repeat bg-[url('https://res.cloudinary.com/dggfjc8rb/image/upload/v1716289115/Dutch%20Strength/DS_Wide_ij2tvb.jpg')] bg-gray-400 bg-blend-multiply bg-cover">
                 <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
                     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">The home of Weightlifting in Amsterdam</h1>
-                    <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Aspiring to become a competitive weightlifter or just want to try out? Come join our community!</p>
+                    <p className="mb-8 text-lg font-normal text-gray-200 lg:text-xl sm:px-16 lg:px-48">Aspiring to become a competitive weightlifter or just want to try out? Come join our community!</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                         <a className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" onClick={handleScrollToPricing}>
-                            Book a sesh
+                            Book a session
                             <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,4 +1,6 @@
 import LinkCard from "../components/linkcard"
+import ShopLanding from "../components/shoplanding"
+
 const items = [
     { name: "Donate", description: "Donate to the Dutch Strength foundation and help improving the facility", link: "https://www.dutchstrength.com/donations-to-the-foundation-dutch-strength/" },
     { name: "Memberships", description: "Buy a 1 time pass or become a member, click to see al your options", link: "https://www.dutchstrength.com/entrance-fees/" },
@@ -11,7 +13,8 @@ const items = [
 export default function Shop() {
     return (
         <>
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <ShopLanding />
+            <div className="mx-auto my-10 max-w-7xl px-6 lg:px-8">
                 <div className="inline-flex flex-wrap mx-auto max-w-7xl px-6 lg:px-8">
                     {items.map((item) => (
                         <LinkCard name={item.name} description={item.description} link={item.link} />
